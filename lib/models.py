6ec1e77c950ce96ca115dad9eedfa5bc5199d843
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2024-02-16 00:15:26 +0300

    completed lab

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,26 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer(), primary_key=True)
+
+    orders = relationship('Order', backref='customer')
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+
+class OrderMetadata(Base):
+    __tablename__ = 'orders_metadata'
+
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey("order.id"))
+
+    order = relationship('Order',
+        backref=backref('order_metadata', uselist=False))    
